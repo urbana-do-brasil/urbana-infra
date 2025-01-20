f1d1102152d094b8891827c0b1693ce3c8787a58
Emanuel Brito <emanuelguimaraes23@gmail.com>
2025-01-20 17:58:43 -0300

    PROD-157 - Inclusão dos valores específicos para os ambientes de Staging e Production

--- terraform/environments/production/variables.tf
+++ terraform/environments/production/variables.tf
@@ -12,19 +12,23 @@ variable "region" {
 variable "cluster_name" {
   type = string
   description = "Nome do cluster GKE para production"
+  default = "urbana-chatbot-production-cluster"
 }
 
 variable "node_count" {
   type = number
   description = "Número de nós no cluster production"
+  default = 3
 }
 
 variable "machine_type" {
   type = string
-  description = "Tipo de máquina dos nós do cluster staging (e.g., e2-medium)"
+  description = "Tipo de máquina dos nós do cluster production (e.g., e2-standard-4)"
+  default = "e2-standard-4"
 }
 
 variable "kubernetes_version" {
   type = string
   description = "Versão do Kubernetes para o cluster production"
+  default = "1.27.1"
 }
--- terraform/environments/staging/variables.tf
+++ terraform/environments/staging/variables.tf
@@ -17,19 +17,23 @@ variable "replicas_api_gateway" {
 variable "cluster_name" {
   type = string
   description = "Nome do cluster GKE para staging"
+  default = "urbana-chatbot-staging-cluster"
 }
 
 variable "node_count" {
   type = number
   description = "Número de nós no cluster staging"
+  default = 1
 }
 
 variable "machine_type" {
   type = string
   description = "Tipo de máquina dos nós do cluster staging (e.g., e2-medium)"
+  default = "e2-medium"
 }
 
 variable "kubernetes_version" {
   type = string
   description = "Versão do Kubernetes para o cluster staging"
+  default = "1.27.1"
 }
