64656016b5f56685ece6a4b5827598aa8cab1828
Emanuel Brito <emanuelguimaraes23@gmail.com>
2025-01-20 14:42:34 -0300

    PROD-152 - Criação do arquivo variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  type = string
+  description = "ID do projeto GCP"
+}
+
+variable "region" {
+  type = string
+  description = "Região do projeto GCP (e.g., us-central1)"
+}
