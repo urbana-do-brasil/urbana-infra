a98e6e1f25bdd5ff0ba1223d40caa1530ddbc1c6
Emanuel Brito <emanuelguimaraes23@gmail.com>
2025-01-20 17:30:23 -0300

    PROD-156 - Inclusão do recurso 'google_container_cluster' nos ambientes de Staging e Production

--- terraform/environments/staging/main.tf
+++ terraform/environments/staging/main.tf
@@ -15,3 +15,15 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+
+resource "google_container_cluster" "primary" {
+  name               = var.cluster_name
+  location           = var.region
+  initial_node_count = var.node_count
+
+  node_config {
+    machine_type = var.machine_type
+  }
+
+  min_master_version = var.kubernetes_version
+}
